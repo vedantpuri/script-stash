619299aecea56db18cb1a345f45d092ea753e5be
Vedant Puri <vedantpuri13@gmail.com>
2018-07-08 20:09:01 +0530

    Refine scring.sh + Update README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# script-stash
-A collection of scripts I made for specific purposes.
+# Script Stash
+A stash of scripts to automate certain specific tasks and make life easy.
+
+## Usage
+Navigate through the root of the repository. Various directories represent the different scripts that are contained within them.
+Open the directory of your choice and read through the '$script_name.md' file to know more about the script
+
+## License
+ The project is available under the **MIT** License. Check out the [license ](https://github.com/vedantpuri/script-stash/blob/master/LICENSE.md) file for more information.
--- Scring/scring.sh
+++ Scring/scring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# script_processor.sh
-# Authors: Vedant Puri
+# scring.sh
+# Author: Vedant Puri
 
 # Text preferences
 underline="$(tput smul)"
@@ -12,11 +12,43 @@ testing=false
 working_dir="$(pwd)/"
 ignore_lines=false
 substr_to_ignore=""
+bash_string=""
 
+# Script information
+script_version="1.0.0"
+
+# Print the script version to console
+print_version() {
+  echo "${script_version}"
+}
+
+# Print scring.sh usage
+print_usage() {
+  echo "Usage: ${bold}./scring.sh${normal} [-v|--version] [-h|--help] [-t|--test] [-i=|--ignore=] [-r=|--relative=] [-a=|--absolute=]
+  where:
+  ${underline}-v${normal}        Prints script version
+  ${underline}-h${normal}        Prints script usage
+  ${underline}-t${normal}        Enable string preview with formatting
+  ${underline}-i=${normal}       Ignore lines containing the string mentioned
+  ${underline}-r=${normal}       Convert script at relative path to string
+  ${underline}-a=${normal}       Convert script at absolute path to string
+
+  ${bold}Note:${normal} Ensure that out of all the options used, the one mentioning the path is specified last."
+}
+
+#  Displays preview of string with formatting if requested
+preview_string() {
+  if [[ "${testing}" == true ]]
+  then
+    echo -e "\n${bold}Script when created will look like:${normal}"
+    echo -e $bash_string
+  fi
+}
+
+# Convert script to string
 script_to_string() {
   local IFS=$'\t'
   local file_path="${1}"
-  local bash_string=""
   while read -r line
   do
     if [[ "${ignore_lines}" == true &&  "${line}" =~ "${substr_to_ignore}" ]]
@@ -29,12 +61,8 @@ script_to_string() {
   echo -e "\n${bold}Your script as a single string is:${normal}"
   bash_string="${bash_string%??}"
   echo  "'"$bash_string"'"
-  # echo
-  if [[ "${testing}" == true ]]
-  then
-    echo -e "\n${bold}Script when created will look like:${normal}"
-    echo -e $bash_string
-  fi
+  preview_string
+
 }
 
 # Parse script arguments
@@ -42,6 +70,12 @@ parse_args() {
   for arg in "${@}"
   do
     case "${arg}" in
+      -v|--version)
+      print_version
+      ;;
+      -h|--help)
+      print_usage
+      ;;
       -r=*|--relative=*)
       local file_path="${working_dir}${arg#*=}"
       script_to_string "${file_path}"
@@ -57,7 +91,8 @@ parse_args() {
       substr_to_ignore="${arg#*=}"
       ;;
       *)
-      echo "Invalid argument."
+      echo "Invalid argument. Run with ${underline}-h${normal} for help."
+      exit
       ;;
     esac
   done
